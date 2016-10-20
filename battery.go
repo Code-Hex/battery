693578b865e38d2241395c527369f63813295b21
K <x00.x7f@gmail.com>
2016-10-20 16:05:29 +0900

    Modified space

--- battery.go
+++ battery.go
@@ -148,7 +148,7 @@ func (bar *Bar) write(frac float64) {
 	if bar.Showthunder {
 		args = append(args, bar.charge)
 	} else {
-		args = append(args, "　")
+		args = append(args, "  ")
 	}
 
 	if bar.ShowPercent {
